2d767db971c61e72ad8141607e9ce375f4f195fa
We10Grad <jonzeldenrust@gmail.com>
2026-01-20 23:01:04 -0600

    Additions to Compute and Network Modules
    Made some changes to the compute module to add work on the asg and made significant changes to network module creating route tables, internet gateway, nat gateway, and associations

--- compute/main.tf
+++ compute/main.tf
@@ -30,49 +30,20 @@ resource "aws_instance" "example" {
 }
 
 # Create a launch configuration for the Auto Scaling group
-resource "aws_placement_group" "test" {
-  name     = "test"
-  strategy = "cluster"
-}
-
-resource "aws_autoscaling_group" "bar" {
+resource "aws_autoscaling_group" "terraform_project_asg" {
   name                      = "terraform_project_asg"
   max_size                  = 5
   min_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 2
-  placement_group           = aws_placement_group.test.id
-  launch_configuration      = aws_launch_configuration.foobar.name
-  vpc_zone_identifier       = [aws_subnet.example1.id, aws_subnet.example2.id]
-
-  instance_maintenance_policy {
-    min_healthy_percentage = 90
-    max_healthy_percentage = 120
-  }
-
-  initial_lifecycle_hook {
-    name                 = "foobar"
-    default_result       = "CONTINUE"
-    heartbeat_timeout    = 2000
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-
-    notification_metadata = jsonencode({
-      foo = "bar"
-    })
-
-    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
-    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
-  }
+  launch_configuration      = aws_launch_configuration.terraform_project_lc.id
+  vpc_zone_identifier       = [aws_subnet.project_public_1.id, aws_subnet.project_public_2.id, aws_subnet.project_public_3.id]
 
   tag {
     key                 = "foo"
     value               = "bar"
     propagate_at_launch = true
-  }
-
-  timeouts {
-    delete = "15m"
   }
 
   tag {
--- networking/main.tf
+++ networking/main.tf
@@ -4,53 +4,109 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "project_public_1"    {
+resource "aws_subnet" "public_subnet_1"    {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_1_cidr
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-  resource "aws_subnet" "project_public_2"    {
+  resource "aws_subnet" "public_subnet_2"    {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_2_cidr
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "project_public_3"    {
+resource "aws_subnet" "public_subnet_3"    {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_3_cidr
   availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "project_private_1" {
+resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_1_cidr
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "project_private_2" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_2_cidr
   availability_zone = "us-east-1b"
 }
 
-resource "aws_route_table" "project_public_1" {
+# Create public route tables and routes
+resource "aws_route_table" "public_route_1" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.public_subnet_1_cidr
-    gateway_id = aws_internet_gateway.main.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Project_IGW.id
   }
 }
 
-resource "aws_route_table" "project_public_2" {
+# Create NAT Gateway
+resource "aws_nat_gateway" "Project_NAT" {
+  allocation_id = aws_eip.Project_NAT_EIP.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "Project NAT"
+  }
+   # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.Project_IGW]
+}
+
+# Create Elastic IP for NAT Gateway
+resource "aws_eip" "Project_NAT_EIP" {
+  domain = "vpc"
+}
+
+
+# Create private route tables and routes
+resource "aws_route_table" "private_route_1" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.public_subnet_2_cidr
-    gateway_id = aws_internet_gateway.main.id
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.Project_NAT.id
   }
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "Project_IGW" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Project IGW"
+  }
+}
+
+# Create Route table associations
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_1.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_1.id
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_route_1.id
+}
+
+resource "aws_route_table_association" "d" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_1.id
+
+  resource "aws_route_table_association" "e" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_1.id
+
+# Security Groups
